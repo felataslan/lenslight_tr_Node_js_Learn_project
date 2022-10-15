28ab7c4577d50f2e6cf1ee8871a7ec976e331522
felat aslan <felataslan58@gmail.com>
2022-10-16 02:12:25 +0300

    Node js learn course subject 3 video 4

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import express  from "express";
 
 const app =express()
-
 const port =3000
+
+//ejs template engine
+app.set('view engine','ejs');
+
+
+//static files middleware
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
-res.send('Index Sayfası 3')
+res.render('index');
+// res.render('about');
+// res.render('blog');
+// res.render('contact');
+// res.render('gallery');
+// res.render('projects');
+// res.render('services');
 })
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
 
 app.listen(port,()=>{
     console.log(`Application Running on port: ${port}`)
