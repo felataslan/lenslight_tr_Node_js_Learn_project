fecd2758324c177c67323311e19b98158b3539f6
felat aslan <felataslan58@gmail.com>
2022-10-18 13:05:41 +0300

    Lesson 4th finished (Controller and Router)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import pageRoute from './route/pageRoute.js'
 
 dotenv.config();
 
@@ -16,13 +17,9 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Routes
+app.use('/',pageRoute)
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
