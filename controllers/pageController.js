a654eb60847628f8d37a2fb8a487b2dd854a64bd
felat aslan <felataslan58@gmail.com>
2022-11-01 18:05:29 +0300

    Homepage design updated and repaired  bug in  getAPhoto function

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,10 +1,21 @@
 import { render } from "ejs";
 import nodemailer from 'nodemailer'
+import Photo from '../models/photoModel.js'
+import User from '../models/userModel.js'
 
 // render indexpage
-const getIndexPage = (req, res) => {
+const getIndexPage = async (req, res) => {
+  const photos = await Photo.find().sort({uploadedAt:-1}).limit(3);
+
+  const numOfUser= await User.countDocuments({})
+  const numOfPhoto= await Photo.countDocuments({})
+
     res.render('index', {
         link: 'index',
+        photos,
+        numOfUser,
+        numOfPhoto,
+
     });
 }
 // render about page
