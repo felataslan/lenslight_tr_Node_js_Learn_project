c9b6016ab70c21199dd30864a9dc59fb4b3e23a7
felat aslan <felataslan58@gmail.com>
2022-11-01 14:46:27 +0300

    Email systems created

--- route/pageRoute.js
+++ route/pageRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 // import * as pageController from '../controllers/pageController.js'
-import {getIndexPage,getAboutPage,getRegisterPage,getLoginPage,getLogout} from '../controllers/pageController.js'
+import {getIndexPage,getAboutPage,getRegisterPage,getLoginPage,getLogout, getContactPage,sendMail} from '../controllers/pageController.js'
 // import * as authMiddleware from '../middlewares/authMiddleware.js'
 
 
@@ -14,6 +14,8 @@ router.route('/about').get(getAboutPage);
 router.route('/register').get(getRegisterPage);
 router.route('/login').get(getLoginPage);
 router.route('/logout').get(getLogout);
+router.route('/contact').get(getContactPage)
+router.route('/contact').post(sendMail)
 
 
 
